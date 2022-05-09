d95db393ecabf7b0b1702f7b990068f57168f3ea
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2022-05-09 07:03:24 -0400

    Add basic aiohttp example

---
+++ examples/aiohttp/README.md
@@ -0,0 +1,12 @@
+# aiohttp example
+
+A simple demo showing form validation with cion, including error handling.
+
+This example uses raw string formatting and in a real application it is heavily advised to use a templating engine like `Jinja2`.
+
+# cion
+This example follows the latest commit of cion, so there may be features that are not included in the latest release.
+Installing the latest commit is as simple as running:
+```bash
+$ pip install git+https://github.com/meizuflux/cion.git
+```
---
+++ examples/aiohttp/server.py
@@ -0,0 +1,81 @@
+from aiohttp import web
+
+import cion
+
+LoginSchema = cion.Schema(
+    fields={
+        "username": cion.Field(
+            type_=cion.types.string(),
+            validators=[cion.validators.length(3, 64)],
+            required=True,
+        ),
+        "password": cion.Field(
+            type_=cion.types.string(),
+            validators=[cion.validators.length(3, 1024)],
+            required=True,
+        ),
+    }
+)
+
+FORM = """
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="/validate_data" method="POST">
+        <input type="text" name="username" placeholder="username">
+        <input type="password" name="password" placeholder="password">
+        <button type="submit">submit</button>
+        <br>
+        {errors}
+    </form>
+</body>
+</html>
+"""
+
+FORM_SUBMIT = """
+<!DOCTYPE html>
+<html>
+<body>
+    <pre>
+Username: {username}
+Password: {password}
+    </pre>
+</body>
+</html>
+"""
+
+
+def index(_: web.Request):
+    return web.Response(body=FORM.format(errors="").encode("utf-8"), content_type="text/html")
+
+
+async def validate_data(request: web.Request):
+    data = await request.post()
+
+    try:
+        validated_data = LoginSchema.validate(dict(data))
+    except cion.ValidationError as error:
+        return web.Response(
+            body=FORM.format(errors="<br>".join(f"{k}: {', '.join(v)}" for k, v in error.errors.items())).encode(
+                "utf-8"
+            ),
+            content_type="text/html",
+        )
+
+    return web.Response(
+        body=FORM_SUBMIT.format(username=validated_data["username"], password=validated_data["password"]).encode(
+            "utf-8"
+        ),
+        content_type="text/html",
+    )
+
+
+def create_app():
+    app = web.Application()
+    app.add_routes([web.get("/", index), web.post("/validate_data", validate_data)])
+
+    return app
+
+
+if __name__ == "__main__":
+    web.run_app(create_app(), host="localhost", port=8080)
